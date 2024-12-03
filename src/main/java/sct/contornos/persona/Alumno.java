28ad345fb16d68c0af808ff29d837ab4f7d754db
a19albertons <a19albertons@iessanclemente.net>
2024-12-03 16:43:42 +0100

    Arreglamos alumno no se había hecho

--- src/main/java/sct/contornos/persona/Alumno.java
+++ src/main/java/sct/contornos/persona/Alumno.java
@@ -7,5 +7,37 @@ package sct.contornos.persona;
  * @author ladmin
  */
 public class Alumno {
+    String codigoXade;
+    boolean autorizaTutores;
+
+    public Alumno() {
+    }
+
+    public Alumno(String codigoXade, boolean autorizaTutores) {
+        this.codigoXade = codigoXade;
+        this.autorizaTutores = autorizaTutores;
+    }
+
+    public String getCodigoXade() {
+        return codigoXade;
+    }
+
+    public void setCodigoXade(String codigoXade) {
+        this.codigoXade = codigoXade;
+    }
+
+    public boolean isAutorizaTutores() {
+        return autorizaTutores;
+    }
+
+    public void setAutorizaTutores(boolean autorizaTutores) {
+        this.autorizaTutores = autorizaTutores;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "codigoXade=" + codigoXade + ", autorizaTutores=" + autorizaTutores + '}';
+    }
+    
     
 }
