61107339fc4805cd35c4bb05b97843c27678a849
Alberto <a19albertons@iessanclemente.net>
2024-12-03 09:17:21 +0100

    Inicial

---
+++ src/main/java/sct/contornos/A19albertons.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sct.contornos;
+
+/**
+ *
+ * @author ladmin
+ */
+public class A19albertons {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
